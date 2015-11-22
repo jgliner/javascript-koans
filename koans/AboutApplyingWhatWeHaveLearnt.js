323b81d5f1f1c76ca33650a6a2deff305b251543
Jarrett Gliner <jarrett.gliner@gmail.com>
2015-11-22 14:56:02 -0800

    Make Range tests pass in AboutApplyingWhatWeHaveLearnt.js
    Self-implemented functional solution uses _.range to generate the array and _.reduce to add values that pass the criteria

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -59,13 +59,15 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.reduce(_.range(1,1000), function(start, val) {    /* try chaining range() and reduce() */
+      return val % 3 === 0 || val % 5 === 0 ? start+val : start;
+    }, 0);
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
